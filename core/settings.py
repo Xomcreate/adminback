9df8b0bd1a63f85c2c212b45c10d869d16514fbe
XomCreate <priscaojimba15@gmail.com>
2026-06-19 04:02:22 -0700

    update

--- core/settings.py
+++ core/settings.py
@@ -139,8 +139,8 @@ EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST          = 'smtp.sendgrid.net'
 EMAIL_PORT          = 587
 EMAIL_USE_TLS       = True
-EMAIL_HOST_USER     = 'apikey'                                   # always literally 'apikey'
-EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')        # your SG.xxx key
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')                            # always literally 'apikey'
+EMAIL_HOST_PASSWORD = os.environ.get('BREVO_API_KEY')        # your SG.xxx key
 DEFAULT_FROM_EMAIL  = os.environ.get('DEFAULT_FROM_EMAIL', 'noreply@example.com')
 
 # ── Misc ──────────────────────────────────────────────────────────────────────
